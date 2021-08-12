79e39d5eb0984a9ada79d7070e66d91640f42dee
Daisho Komiyama <daisho.komiyama@fabric.inc>
2021-08-12 13:50:13 -0400

    Add image using image tag as Image coponent is not compatible with 'next export'

--- components/header.js
+++ components/header.js
@@ -1,19 +1,20 @@
 import { Button, Pane, Text, majorScale } from 'evergreen-ui'
-import Image from 'next/image'
+// import Image from 'next/image'
 import React from 'react'
 import styles from 'styles/Header.module.scss'
+// import logo from './lps-logo.png'
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Image
+      <img
         className='logo'
-        src='/lps-logo.png'
+        src='/static/lps-logo.png'
         alt='standing cowboy cat'
         width='150'
         height='150'
       />
-      <h1>Last Person Standing V2</h1>
+      <h1>Last Person Standing V3</h1>
     </header>
   )
 }
