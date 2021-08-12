34401979c370616b0a0777b6f9fde9058ce67149
Daisho Komiyama <daisho.komiyama@fabric.inc>
2021-08-12 13:04:20 -0400

    Add the logo

--- components/header.js
+++ components/header.js
@@ -1,10 +1,17 @@
 import { Button, Pane, Text, majorScale } from 'evergreen-ui'
+import Image from 'next/image'
 import React from 'react'
 import styles from 'styles/Header.module.scss'
 
 const Header = () => {
   return (
     <header className={styles.header}>
+      <Image
+        src='/lps-logo.png'
+        alt='standing cowboy cat'
+        width='128'
+        height='128'
+      />
       <h1>Last Person Standing V2</h1>
     </header>
   )
