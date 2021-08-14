3e0f338dbab61c049bafc4fecc636d092c9ed041
daisho <daishokomiyama@gmail.com>
2021-08-14 15:42:41 -0400

    Clean up

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 import Container from 'components/container'
 import Head from 'components/head'
 import Header from 'components/header'
-import Link from 'next/link'
-import { Button, Pane, Text, majorScale } from 'evergreen-ui'
 import Landing from 'components/landing'
-import { useRouter } from 'next/router'
 
 const Main = () => {
-  // const router = useRouter()
-  // const change = () => {
-  //   const url = '/book'
-  //   router.push(url, null, { shallow: true })
-  // }
   return (
     <>
       <Head />
