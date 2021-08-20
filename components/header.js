280dad0e24f28964bd73ec91602abc582aef447c
Daisho Komiyama <daisho.komiyama@fabric.inc>
2021-08-20 00:05:00 -0400

    Correct build script

--- components/header.js
+++ components/header.js
@@ -1,19 +1,23 @@
-import { header, inner } from 'styles/Header.module.scss'
+import { header, inner, left, right } from 'styles/Header.module.scss'
 import Image from 'next/image'
 import React from 'react'
+import Sidebar from 'components/sidebar'
 import cn from 'classnames'
 
 const Header = () => {
   return (
     <header className={header}>
       <div className={cn('wrapper', inner)}>
-        <Image
-          src='https://lps-images.s3.amazonaws.com/lps-logo.png'
-          alt='standing cowcat'
-          height='175'
-          width='175'
-        />
-        <h1>Last Person Standing</h1>
+        <div className={left}>
+          <Image
+            src='https://lps-images.s3.amazonaws.com/lps-logo.png'
+            alt='standing cowcat'
+            height='175'
+            width='175'
+          />
+          <h1>Last Person Standing</h1>
+        </div>
+        <Sidebar className={right} />
       </div>
     </header>
   )
--- components/main.js
+++ components/main.js
@@ -1,6 +1,6 @@
 import Buttons from 'components/buttons'
 import React from 'react'
-import Sidebar from 'components/sidebar'
+// import Sidebar from 'components/sidebar'
 import styles from 'styles/Main.module.scss'
 
 const Main = () => {
@@ -9,7 +9,7 @@ const Main = () => {
       <div className={styles.mainContents}>
         <Buttons />
       </div>
-      <Sidebar />
+      {/* <Sidebar /> */}
     </section>
   )
 }
